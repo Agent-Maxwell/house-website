fa4597c42642fb5852f7d012292443cb1bd068cd
Maxwell <avonbraunbehrens@gmail.com>
2023-05-04 02:18:51 -0600

    huh?

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ template.innerHTML = `
 <div class="navbar">
 <a class="active" href="index.html">Home</a>
 <a href="faculty.html">Faculty</a>
+<a href="comments.html">Comments</a>
 <a href="paulsroom.html">Paul's Room</a>
 <a href="adriansroom.html">Adrian's Room</a>
 <a href="lilysroom.html">Lily's Room</a>
