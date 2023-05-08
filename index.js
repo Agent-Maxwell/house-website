8dcfbb4b4fb00d7276a53d792c515fee6fa86e6d
Paul Nuffer <78831963+PaulNuffer@users.noreply.github.com>
2023-05-08 16:59:51 -0600

    header should work and relative paths fixed

--- index.js
+++ index.js
@@ -2,13 +2,13 @@ const template = document.createElement('template');
 
 template.innerHTML = `
 <div class="navbar">
-<a class="active" href="index.html">Home</a>
-<a href="faculty.html">Faculty</a>
-<a href="comments.html">Comments</a>
-<a href="paulsroom.html">Paul's Room</a>
-<a href="adriansroom.html">Adrian's Room</a>
-<a href="lilysroom.html">Lily's Room</a>
-<a href="davidpages/davidsroom.html">davids room</a>
+<a class="active" href="https://gayhouse.online/index.html">Home</a>
+<a href="https://gayhouse.online/faculty.html">Faculty</a>
+<a href="https://gayhouse.online/comments.html">Comments</a>
+<a href="https://gayhouse.online/paulpages/paulsroom.html">Paul's Room</a>
+<a href="https://gayhouse.online/adrianpages/adriansroom.html">Adrian's Room</a>
+<a href="https://gayhouse.online/lilypages/lilysroom.html">Lily's Room</a>
+<a href="https://gayhouse.online/davidpages/davidsroom.html">davids room</a>
 </div>
 
 <br>
