facd3796838255655ef79c51a3248bb068754e8a
volohov <57515316+Volohov7@users.noreply.github.com>
2026-04-11 15:27:54 -0600

    homepage update
    finally figured out latest youtube video embed. now provided in an immersive theater experience

--- index.js
+++ index.js
@@ -1,20 +1,18 @@
-const template = document.createElement('template');
+const navbar = document.createElement('template');
 
-template.innerHTML = `
+navbar.innerHTML = `
 <div class="navbar">
-<a href="/index.html">Home</a>
-<a href="/faculty/faculty.html">Faculty</a>
-<a href="/comments.html">Comments</a>
-<a href="/paulpages/paulsroom.html">Paul's Room</a>
-<a href="/adrianpages/adriansroom.html">Adrian's Room</a>
-<a href="/lilypages/lilysroom.html">Lily's Room</a>
-<a href="/davidpages/index.html">David's Room</a>
-<a href="/avapages/index.html">Ava's Room</a>
-<a href="/opheliapages/index.html">Ophelia's Room</a>
-<a href="/gavinpages/index.html">Gavin's Room</a>
+    <a href="/index.html">Home</a>
+    <a href="/faculty/faculty.html">Faculty</a>
+    <a href="/comments.html">Comments</a>
+    <a href="/paulpages/paulsroom.html">Paul's Room</a>
+    <a href="/adrianpages/adriansroom.html">Adrian's Room</a>
+    <a href="/lilypages/lilysroom.html">Lily's Room</a>
+    <a href="/davidpages/index.html">David's Room</a>
+    <a href="/avapages/index.html">Ava's Room</a>
+    <a href="/opheliapages/index.html">Ophelia's Room</a>
+    <a href="/gavinpages/index.html">Gavin's Room</a>
 </div>
-
-<br>
 `;
 
-document.body.prepend(template.content);
+document.body.prepend(navbar.content);
