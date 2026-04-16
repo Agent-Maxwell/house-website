974f0b05910adedd3e6152985f8adfc13f50985b
volohov <57515316+Volohov7@users.noreply.github.com>
2026-04-16 10:43:03 -0600

    ethan neocity linked
    link to ethans neo-city; 4h; form test; games update (slapjack,spit); slap:"honor"

---
+++ davidpages/blog/test/test.js
@@ -0,0 +1,16 @@
+function getData(form) {
+  var formData = new FormData(form);
+
+  for (var pair of formData.entries()) {
+    console.log(pair[0] + ": " + pair[1]);
+  }
+
+  console.log(Object.fromEntries(formData));
+}
+
+document.getElementById("myForm").addEventListener("submit", function (e) {
+  e.preventDefault();
+  getData(e.target);
+//   document.getElementById("question").innerText = "are you more of a red or blue persona?"
+});
+
