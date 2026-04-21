7bb5ff387dc8225414e165a6c431a86561bb68c3
volohov <57515316+Volohov7@users.noreply.github.com>
2026-04-21 13:51:59 -0600

    ships
    ethan link icon. uqm fixed. fit experimenting. added "sort by: random." working on ship entry styling, kohr-ah entry.

--- davidpages/uqm/index.js
+++ davidpages/uqm/index.js
@@ -4,180 +4,181 @@ const ships = [
         name: "Androsynth",
         crew: 20,
         batt: 24,
-        score: 15,
+        value: 15,
     },
     {
         nameOrder: 2,
         name: "Arilou",
         crew: 6,
         batt: 20,
-        score: 16,
+        value: 16,
     },
     {
         nameOrder: 3,
         name: "Chenjesu",
         crew: 36,
         batt: 30,
-        score: 28,
+        value: 28,
     },
     {
         nameOrder: 4,
         name: "Chmmr",
         crew: 42,
         batt: 42,
-        score: 30,
+        value: 30,
     },
     {
         nameOrder: 5,
         name: "Druuge",
         crew: 14,
         batt: 32,
-        score: 17,
+        value: 17,
     },
     {
         nameOrder: 6,
         name: "Earthling",
         crew: 18,
         batt: 18,
-        score: 11,
+        value: 11,
     },
     {
         nameOrder: 7,
         name: "Ilwrath",
         crew: 22,
         batt: 16,
-        score: 10,
+        value: 10,
     },
     {
         nameOrder: 8,
         name: "Kohr-Ah",
         crew: 42,
         batt: 42,
-        score: 30,
+        value: 30,
     },
     {
         nameOrder: 9,
         name: "Melnorme",
         crew: 20,
         batt: 42,
-        score: 18,
+        value: 18,
     },
     {
         nameOrder: 10,
         name: "Mmrnmhrm",
         crew: 20,
         batt: 10,
-        score: 19,
+        value: 19,
     },
     {
         nameOrder: 11,
         name: "Mycon",
         crew: 20,
         batt: 40,
-        score: 21,
+        value: 21,
     },
     {
         nameOrder: 12,
         name: "Orz",
         crew: 16,
         batt: 20,
-        score: 23,
+        value: 23,
     },
     {
         nameOrder: 13,
         name: "Pkunk",
         crew: 8,
         batt: 12,
-        score: 20,
+        value: 20,
     },
     {
         nameOrder: 14,
         name: "Shofixti",
         crew: 6,
         batt: 4,
-        score: 5,
+        value: 5,
     },
     {
         nameOrder: 15,
         name: "Slylandro",
         crew: 12,
         batt: 20,
-        score: 17,
+        value: 17,
     },
     {
         nameOrder: 16,
         name: "Spathi",
         crew: 30,
         batt: 10,
-        score: 18,
+        value: 18,
     },
     {
         nameOrder: 17,
         name: "Supox",
         crew: 12,
         batt: 16,
-        score: 16,
+        value: 16,
     },
     {
         nameOrder: 18,
         name: "Syreen",
         crew: 12, // syreen funny, max 42
         batt: 16,
-        score: 13,
+        value: 13,
     },
     {
         nameOrder: 19,
         name: "Thraddash",
         crew: 8,
         batt: 24,
-        score: 10,
+        value: 10,
     },
     {
         nameOrder: 20,
         name: "Umgah",
         crew: 10,
         batt: 30,
-        score: 7,
+        value: 7,
     },
     {
         nameOrder: 21,
         name: "Ur-Quan",
         crew: 42,
         batt: 42,
-        score: 30,
+        value: 30,
     },
     {
         nameOrder: 22,
         name: "Utwig",
         crew: 20,
         batt: 10, // utwig funny, max 20
-        score: 22,
+        value: 22,
     },
     {
         nameOrder: 23,
         name: "Vux",
         crew: 20,
         batt: 40,
-        score: 12,
+        value: 12,
     },
     {
         nameOrder: 24,
         name: "Yehat",
         crew: 20,
         batt: 10,
-        score: 23,
+        value: 23,
     },
     {
         nameOrder: 25,
         name: "ZoqFot",
         crew: 10,
         batt: 10,
-        score: 6,
+        value: 6,
     }
 ]
 
 let order = "ascending";
-let currentSortBy = "nameOrder";
+let currentSortBy = null;
+sortBy("nameOrder");
 
 // set ship img src's
 for (ship of ships) {
@@ -197,19 +198,7 @@ function sortBy(what) {
     for (ship of ships) {
         const shipImg = document.getElementById(ship.name.toLowerCase());
 
-        // NAME ORDER
-        // shipImg.style.order = ship.nameOrder;
-        // SCORE ORDER
-        // shipImg.style.order = ship.score;
-        // CREW ORDER
-        // shipImg.style.order = ship.crew;
-        // BATT ORDER
-        // shipImg.style.order = ship.batt;
-
-        // WHAT order
-
-        //note: setting the order for the img's parent element, the <a>'s that are actually being ordered
-
+        // set the CSS order of the shipimg parents, in WHAT order
         switch (order) {
             case "ascending":
                 shipImg.parentElement.style.order = ship[currentSortBy];
@@ -220,12 +209,38 @@ function sortBy(what) {
             default:
                 console.log("invalid ordering: " + order);
         }
+
+        // also set image hover text to show info
+        switch (currentSortBy) {
+            case "nameOrder":
+                shipImg.title = ship.name;
+                break;
+            case "crew":
+                shipImg.title = "Crew: " + ship[currentSortBy];
+                break;
+            case "batt":
+                shipImg.title = "Battery: " + ship[currentSortBy];
+                break;
+            case "value":
+                shipImg.title = "Point Value: " + ship[currentSortBy];
+                break;
+            case "random":
+                shipImg.title = "Random Index: " + ship[currentSortBy];
+                break;
+            default:
+                shipImg.title = "";
+                console.log("unknown sort by: " + currentSortBy + ". -sincerely, the hover text guys");
+        }
     }
 }
 // make each button call sortBy w corresponding value
+// (click instead of change, so you can repeatedly shuffle)
 sortByButtons.forEach(button => {
-    button.addEventListener('change', (e) => {
+    button.addEventListener('click', (e) => {
         if (e.target.checked) {
+            // when Random clicked, assign new random indices
+            if (e.target.value == "random") {shuffle()};
+            // then re-sort
             sortBy(e.target.value);
         }
     })
@@ -239,19 +254,15 @@ function sizeChange(value) {
     // fit or number?
     switch (value) {
         case "fit":
-            // const sizeButtons = document.getElementById('');
-
-            // r.style.setProperty('--pixel-size', value);
-            // r.style.setProperty('max-width', value);
-            document.getElementById("ships-box").style.setProperty("max-width", "100%");
-            document.getElementById("ships-box").style.setProperty("width", "auto");
+            // fit: add class "fit"
+            document.getElementById("ships-box").classList.add("fit");
             break;
         default:
-            // r.style.setProperty('--pixel-size', value + 'px');
-            r.style.setProperty('--pixel-size', window.screen.width / (65 * 5 + 4));
+            // number: set pixel size variable, clear fit
+            document.getElementById("ships-box").classList.remove("fit");
+            r.style.setProperty('--pixel-size', value + 'px');
     }
 
-    r.style.setProperty('--pixel-size', value + 'px');
 }
 // make each radio button call sizeChange w corresponding value
 sizeButtons.forEach(button => {
@@ -274,3 +285,26 @@ orderButtons.forEach(button => {
     })
 });
 
+
+
+
+// press F to show/hide experimental "fit" option
+document.addEventListener("keydown", (e) => {
+    if (e.code == "KeyF") {
+        const fitOption = document.getElementById("fit-option");
+
+        if (fitOption.style.display != "inline") {
+            fitOption.style.display = "inline";
+        } else if (fitOption.style.display == "inline") {
+            fitOption.style.display = "none";
+        }
+    }
+});
+
+// shuffle ships anew (only when random clicked.)
+function shuffle() {
+    for (ship of ships) {
+        // 0-99
+        ship.random = Math.floor(100 * Math.random());
+    }
+}
--- davidpages/uqm/ship.js
+++ davidpages/uqm/ship.js
@@ -7,38 +7,42 @@ let ship = params.get("id");
 
 async function loadPage() {
 
-    // todo check if ship name is valid
-    if (ship) {
-        // tab name
-        document.title = ship;
-        // image
-        document.getElementById("ship-img").src = "img/ship-stats/" + ship + ".png";
-        document.getElementById("ship-img").style.imageRendering = "pixelated";
+    // todo check if ship name is valid?
+    if (true) {
+        // tab icon
+        const faviconLink = document.querySelector("link[rel~='icon']");
+        faviconLink.href = "/davidpages/uqm/img/ship-icons/" + ship + ".png";
 
-        // get ship entry html
+        // tab name
+        document.title = capitalizeShipName(ship);
+
+        // ship-img
+        document.getElementById("ship-img").src = "img/ship-stats/" + ship + ".png";
+
+        // get ship-content html
         let rawHTML = null;
-        const idHtmlResponse = await fetch("html/" + ship + ".html");
-        if (idHtmlResponse.status === 404) {
+        const response = await fetch("html/" + ship + ".html");
+        if (response.status === 404) {
             console.log("missing html for ship " + ship);
             const response = await fetch("html/default.html");
             rawHTML = await response.text();
         } else {
             // get raw html from the [id].html file
-            rawHTML = await idHtmlResponse.text();
+            rawHTML = await response.text();
         }
-
-        // RENDER PAGE for VALID ID -------------------------------------------------
-
+        // apply ship-content html
         document.getElementById("ship-content").innerHTML += rawHTML;
 
-        // WIKI LINK
-        const wikiLink = document.getElementById("link");
-        wikiLink.href = "https://wiki.uqm.stack.nl/" + ship;
-        document.body.appendChild(wikiLink);
 
-        // const wikiLink = document.getElementById("wiki-link");
+
+        // wiki link
+        // note: slightly broken for some, possibly just ur-quan & kohr-ah
+        const wikiLink = document.getElementById("wiki-link");
+        wikiLink.href = "https://wiki.uqm.stack.nl/" + ship;
+        document.getElementById("ship-content").appendChild(wikiLink);
 
     } else {
+        // note: not happening rn
         // RENDER PAGE for INVALID ID------------------------------------------------
 
         console.error("invalid id: " + id);
@@ -51,6 +55,29 @@ async function loadPage() {
     }
 }
 
+// FUNCTIONS
+
+function capitalizeShipName(id) {
+    // capitalization exception: ZoqFot
+    if (id == "zoqfot") {
+        return "ZoqFot";
+    }
+    return toTitleCase(id);
+}
+
+// Source - https://stackoverflow.com/a/196991
+// Posted by Greg Dean, modified by community. See post 'Timeline' for change history
+// Retrieved 2026-04-21, License - CC BY-SA 4.0
+function toTitleCase(str) {
+    return str.replace(
+        /\w*/g,
+        text => text.charAt(0).toUpperCase() + text.substring(1).toLowerCase()
+    );
+}
+
+//////////////////////////////////////////////////////////////////
+
+// actually load the page
 await loadPage();
 
 // duplicating standard-style behaviour here for now...
